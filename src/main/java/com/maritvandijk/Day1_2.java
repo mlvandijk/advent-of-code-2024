23fd2a04f531ceb51f5098728558db1e929d63b9
marit.van.dijk <mlvandijk@gmail.com>
2024-12-01 16:28:52 +0100

    Refactor solution for day 1, puzzle 2

--- src/main/java/com/maritvandijk/Day1_2.java
+++ src/main/java/com/maritvandijk/Day1_2.java
@@ -3,10 +3,9 @@ package com.maritvandijk;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day1_2 {
     public static void main(String[] args) throws IOException {
@@ -19,19 +18,17 @@ public class Day1_2 {
             });
         }
 
-        Map<Integer, Integer> frequency = new HashMap<>();
-        for (int value : right) {
-            if (frequency.containsKey(value)) {
-                frequency.compute(value, (key, count) -> count + 1);
-            } else {
-                frequency.put(value, 1);
-            }
-        }
+        HashMap<Integer, Long> frequency =
+                right.stream().collect(Collectors.groupingBy(
+                        Function.identity(), HashMap::new,
+                        Collectors.counting()
+                ));
+
 
         int similarityScore = 0;
         for (Integer integer : left) {
             if (frequency.containsKey(integer)) {
-                int similarity = integer * frequency.get(integer);
+                int similarity = (int) (integer * frequency.get(integer));
                 similarityScore += similarity;
             }
         }
